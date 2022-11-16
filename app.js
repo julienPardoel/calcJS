28f18d07c8a0cf24e863bd42d3099d0c7cc00cae
julien <julien.pardoel@gmail.com>
2022-11-16 21:22:39 +0100

    calcul

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-console.log("hello");
+// console.log("hello");
 
 const touches = [...document.querySelectorAll(".bouton")];
-console.log(touches);
+// console.log(touches);
+
+const listeKeycode = touches.map((touche) => touche.dataset.key);
+// console.log(listeKeycode);
+
+const ecran = document.querySelector(".ecran");
+
+document.addEventListener("keydown", (e) => {
+  const valeur = e.keyCode.toString();
+  //   console.log(valeur);
+  calculer(valeur);
+});
+
+document.addEventListener("click", (e) => {
+  const valeur = e.target.dataset.key;
+  //   console.log(valeur);
+  calculer(valeur);
+});
+
+const calculer = (valeur) => {
+  if (listeKeycode.includes(valeur)) {
+    // console.log(listeKeycode);
+    switch (valeur) {
+      case "8":
+        ecran.textContent = "";
+        break;
+      case "13":
+        const calcul = eval(ecran.textContent);
+        ecran.textContent = calcul;
+        break;
+      default:
+        const indexKeycode = listeKeycode.indexOf(valeur);
+        const touche = touches[indexKeycode];
+        ecran.textContent += touche.innerHTML;
+    }
+  }
+};
